54b05b6afddcf2c84f62b74bae92218336c3bd40
candroid <cnrture@gmail.com>
2025-07-17 01:38:40 +0300

    Update plugin management for Compose and toolchain versioning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 rootProject.name = "KMaestro"
+
+pluginManagement {
+    plugins {
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    }
+}
